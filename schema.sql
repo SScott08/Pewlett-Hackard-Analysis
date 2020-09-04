9ff477b3ead63cedbdae14b0542176e4bb6e84e6
SScott08 <68204195+SScott08@users.noreply.github.com>
2020-09-04 14:41:02 -0700

    message

--- schema.sql
+++ schema.sql
@@ -16,15 +16,6 @@ CREATE TABLE departments (
      PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_managers (
-dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
-);
 
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
@@ -36,20 +27,60 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE titles (
-  emp_no INT NOT NULL,
-  title VARCHAR NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+emp_no INT NOT NULL,
+title VARCHAR(50) NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
+PRIMARY KEY (emp_no, title, from_date)
 );
 
 CREATE TABLE dept_emp (
+  emp_no INT  NOT NULL,
   dept_no VARCHAR NOT NULL,
-  emp_no VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
   PRIMARY KEY (emp_no, dept_no)
 );
 
 SELECT * FROM dept_emp;
+
+skill drill
+SELECT departments.dept_name,
+     dept_managers.emp_no,
+     dept_managers.from_date,
+     dept_managers.to_date
+FROM departments
+LEFT JOIN dept_managers
+ON departments.dept_no = dept_managers.dept_no;
+
+
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+--INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+ 	AND (de.to_date = '9999-01-01');
+
+  -- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+--INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
